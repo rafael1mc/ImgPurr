1449a4bf0051afa4428ebd0422513a53ee739f42
rafaelcosta <rafael1_mc@hotmail.com>
2019-11-13 15:32:32 -0300

    Expanding on ListItemModel to parse from another type

--- app/src/main/java/com/example/imgpurr/screen/imgur/ListItemModel.kt
+++ app/src/main/java/com/example/imgpurr/screen/imgur/ListItemModel.kt
@@ -1,3 +1,19 @@
 package com.example.imgpurr.screen.imgur
 
-class ListItemModel(val url: String)
+import com.example.imgpurr.repository.entity.GalleryResponseModel
+
+class ListItemModel(val url: String) {
+    companion object {
+
+        fun fromGalleryResponseModel(item: GalleryResponseModel): List<ListItemModel> {
+            val list = mutableListOf<ListItemModel>()
+            if (item.isAlbum) {
+                list.addAll(item.images?.map { ListItemModel(it.link) } ?: listOf())
+            } else {
+                list.add(ListItemModel(item.link ?: ""))
+            }
+            return list
+        }
+
+    }
+}
