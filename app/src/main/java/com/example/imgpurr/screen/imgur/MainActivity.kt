9d034105efdd724edd18b4de2ba528d6c9723fd6
rafaelcosta <rafael1_mc@hotmail.com>
2019-11-13 15:39:55 -0300

    Calling viewModel to load data and reacting to changes

--- app/src/main/java/com/example/imgpurr/screen/imgur/MainActivity.kt
+++ app/src/main/java/com/example/imgpurr/screen/imgur/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.imgpurr.screen.imgur
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.imgpurr.R
 import com.example.imgpurr.util.EndlessRecyclerOnScrollListener
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,10 +23,12 @@ class MainActivity : AppCompatActivity() {
 
         prepareObservables()
         prepareComponents()
+
+        viewModel.load(fromBeginning = true)
     }
 
     private fun prepareObservables() {
-
+        viewModel.model.stateOb.observe(this, Observer(::handleState))
     }
 
     private fun prepareComponents() {
@@ -42,6 +48,40 @@ class MainActivity : AppCompatActivity() {
 
         materialButtonRetry.setOnClickListener {
             viewModel.load(fromBeginning = true)
+        }
+    }
+
+    private fun handleState(state: MainModel.ImgurState) {
+        when (state) {
+            is MainModel.ImgurState.Loading -> {
+                groupError.visibility = View.GONE
+                progressBar.visibility = View.VISIBLE
+            }
+            is MainModel.ImgurState.Loaded -> {
+                progressBar.visibility = View.GONE
+                groupError.visibility = View.GONE
+                recyclerViewImages.visibility = View.VISIBLE
+
+                (recyclerViewImages.adapter as MainAdapter)
+                    .addItems(state.searchResult ?: listOf())
+            }
+            is MainModel.ImgurState.Error -> {
+                progressBar.visibility = View.GONE
+                val adapter = recyclerViewImages.adapter as MainAdapter
+                if (adapter.items.isEmpty()) {
+                    recyclerViewImages.visibility = View.GONE
+                    groupError.visibility = View.VISIBLE
+
+                    textViewErrorMessage.text = state.message
+                    Log.d("rafadebug", state.message)
+                    onScrollListener.reset()
+                } else {
+                    Snackbar.make(constraintLayoutParent, state.message, Snackbar.LENGTH_LONG)
+                        .show()
+                    onScrollListener.reset()
+                }
+
+            }
         }
     }
 
