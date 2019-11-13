22c468caaeb032c1ab596ea128a3642b9bc6cfb9
rafaelcosta <rafael1_mc@hotmail.com>
2019-11-13 15:36:43 -0300

    Preparing components on MainActivity

--- app/src/main/java/com/example/imgpurr/screen/imgur/MainActivity.kt
+++ app/src/main/java/com/example/imgpurr/screen/imgur/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.imgpurr.screen.imgur
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.imgpurr.R
+import com.example.imgpurr.util.EndlessRecyclerOnScrollListener
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<MainViewModel>()
+    private lateinit var onScrollListener: EndlessRecyclerOnScrollListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareComponents() {
+        onScrollListener =
+            object : EndlessRecyclerOnScrollListener(SPAN_COUNT * IMAGES_THRESHOLD_COUNT) {
+                override fun onLoadMore(currentPage: Int) {
+                    viewModel.load()
+                }
+            }
+        recyclerViewImages.apply {
+            layoutManager = GridLayoutManager(context, SPAN_COUNT)
 
+            adapter = MainAdapter()
+
+            addOnScrollListener(onScrollListener)
+        }
+
+        materialButtonRetry.setOnClickListener {
+            viewModel.load(fromBeginning = true)
+        }
+    }
+
+    companion object {
+        private const val IMAGES_THRESHOLD_COUNT = 5
+        private const val SPAN_COUNT = 4
     }
 }
